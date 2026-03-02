1576ba5d5841c2acb2751ad0f5e049529057e674
JamesSerenio <sereniojames363@gmail.com>
2026-03-02 11:08:33 +0800

    added controller in admin mapping

---
+++ app/Http/Controllers/admin/AdminMapController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class AdminMapController extends Controller
+{
+    public function index()
+    {
+        return view('pages.admin.mapping');
+    }
+}
