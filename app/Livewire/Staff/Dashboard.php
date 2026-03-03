9963d5baacc907ba43e6fc999371c362618021b2
JamesSerenio <sereniojames363@gmail.com>
2026-03-03 08:41:58 +0800

    added some code in blade

---
+++ app/Livewire/Staff/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Staff;
+
+use Livewire\Component;
+
+class Dashboard extends Component
+{
+    public function render()
+    {
+        // since your view is in resources/views/components/staff/dashboard.blade.php
+        return view('components.staff.dashboard');
+    }
+}
