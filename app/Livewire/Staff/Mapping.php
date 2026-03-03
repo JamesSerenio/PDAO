77046b145330b6ff4d33ad9674ff856e69089f7c
JamesSerenio <sereniojames363@gmail.com>
2026-03-03 08:42:14 +0800

    added some code in blade

---
+++ app/Livewire/Staff/Mapping.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Staff;
+
+use Livewire\Component;
+
+class Mapping extends Component
+{
+    public function render()
+    {
+        return view('components.staff.mapping');
+    }
+}
