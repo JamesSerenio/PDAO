16019ce153c7b5bcf1903f06e72c5cffdbf3a393
JamesSerenio <sereniojames363@gmail.com>
2026-05-19 09:09:45 +0800

    added vission.blade.php

---
+++ resources/views/pages/get-to-know-us/vision.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="simple-section vision-bg">
+
+    <div class="simple-container">
+        <h1 class="section-title">Vision</h1>
+
+        <div class="card-glow">
+            <p>
+                We envision a community for people with disabilities that is inclusive,
+                empowered, self reliant, productive, and disaster resilient.
+            </p>
+        </div>
+    </div>
+
+</section>
+
+@endsection
