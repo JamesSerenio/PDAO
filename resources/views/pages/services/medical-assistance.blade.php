1bf60b1180538876ae8dc132415c6d9dd4063eed
JamesSerenio <sereniojames363@gmail.com>
2026-05-19 09:11:54 +0800

    added medical-assistance.blade.php

---
+++ resources/views/pages/services/medical-assistance.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<section class="simple-section"><div class="simple-container"><h1 class="section-title">Medical Assistance</h1><div class="card-glow"><p>Medical Assistance page.</p></div></div></section>
+@endsection
