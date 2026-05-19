4b6b13a8ffdfa3379f03d27c19e8cad8efd1855c
JamesSerenio <sereniojames363@gmail.com>
2026-05-19 09:10:55 +0800

    added livelihood-programs.blade.php Copy path

---
+++ resources/views/pages/services/livelihood-programs.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<section class="simple-section"><div class="simple-container"><h1 class="section-title">Livelihood Programs</h1><div class="card-glow"><p>Livelihood Programs page.</p></div></div></section>
+@endsection
