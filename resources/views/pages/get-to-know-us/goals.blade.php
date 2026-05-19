86402c3bc42f7d65388249759e232269434de905
JamesSerenio <sereniojames363@gmail.com>
2026-05-19 09:07:25 +0800

    added goals.blade.php

---
+++ resources/views/pages/get-to-know-us/goals.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="simple-section goals-bg">
+
+    <div class="simple-container">
+        <h1 class="section-title">Goals</h1>
+
+        <div class="goals-grid">
+
+            <div class="goal-card">
+                <i class="fa-solid fa-scale-balanced"></i>
+                <h3>Productive</h3>
+            </div>
+
+            <div class="goal-card">
+                <i class="fa-solid fa-users"></i>
+                <h3>Resilient</h3>
+            </div>
+
+            <div class="goal-card">
+                <i class="fa-solid fa-hand-holding-heart"></i>
+                <h3>Empowered</h3>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+@endsection
