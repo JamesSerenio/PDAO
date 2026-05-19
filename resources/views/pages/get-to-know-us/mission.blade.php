ba855c10c76d84028fee705c1ac2964b91f93656
JamesSerenio <sereniojames363@gmail.com>
2026-05-19 09:08:14 +0800

    added mission.blade.php

---
+++ resources/views/pages/get-to-know-us/mission.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="simple-section mission-bg">
+
+    <div class="simple-container">
+        <h1 class="section-title">Mission</h1>
+
+        <div class="card-glow">
+            <p>
+                To implement programs, and services that would introduce evidence-based and
+                community-based approaches that will be more responsive to person with
+                disabilities resident of municipality in accordance with the municipal
+                governments inclusive development and good governance agenda.
+            </p>
+        </div>
+    </div>
+
+</section>
+
+@endsection
